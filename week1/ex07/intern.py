016cedfafa1fb48a7cb0dfabc1975f196d335cfb
Francesco Frau <ffrau@e3r9p1.42roma.it>
2022-05-17 20:01:22 +0200

    Update v1.0.3-Alpha
    - Fixed exercise 07

--- week1/ex07/intern.py
+++ week1/ex07/intern.py
@@ -1,6 +1,3 @@
-from os import PRIO_USER
-
-
 class Intern:
 	name = "My name? I'm nobody, an intern, I have no name."
 	def	builder(self, name):
@@ -9,21 +6,19 @@ class Intern:
 
 	def	__str__(self):
 		return self.name
-
-class Coffee:
-	def	__str__(self):
-		return "This is the worst coffee you ever tasted."
 	
 	def	work(self):
 		raise Exception("I'm just an intern, I can't do that...")
 	
 	def	make_coffee(self):
-		return self
-	
+		return self.Coffee()
+	class Coffee:
+		def	__str__(self):
+			return "This is the worst coffee you ever tasted."
 
 
 intern = Intern()
-coffee = Coffee().make_coffee()
+coffee = intern.Coffee.make_coffee
 intern.builder("Giovanni")
 print(intern.__str__())
 print(coffee.__str__())
